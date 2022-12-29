b8097d5271db647258977edc897df313ccac0466
franeum <elettrochuck@gmail.com>
2022-12-29 21:43:22 +0100

    ADD

---
+++ study/reascript01.py
@@ -0,0 +1,28 @@
+RGN_DUR = 0.1
+
+
+def add_region(name, pos, idx=-1, typ=False):
+    RPR_AddProjectMarker(None, typ, pos, pos+RGN_DUR, name, idx)
+
+
+add_region('marka', 0.018140589569160998, typ=False)
+add_region('marka', 0.531156462585034, typ=False)
+add_region('marka', 1.0499773242630386, typ=False)
+add_region('marka', 1.3249886621315192, typ=False)
+add_region('marka', 3.0026303854875285, typ=False)
+add_region('marka', 3.506938775510204, typ=False)
+add_region('marka', 3.80734693877551, typ=False)
+add_region('marka', 4.3290702947845805, typ=False)
+add_region('marka', 4.915374149659864, typ=False)
+add_region('marka', 5.390657596371882, typ=False)
+add_region('marka', 5.706303854875284, typ=False)
+add_region('marka', 6.17795918367347, typ=False)
+add_region('marka', 7.0436281179138325, typ=False)
+add_region('marka', 7.355646258503401, typ=False)
+add_region('marka', 7.778684807256236, typ=False)
+add_region('marka', 8.090702947845806, typ=False)
+add_region('marka', 8.413605442176872, typ=False)
+add_region('marka', 8.728526077097506, typ=False)
+add_region('marka', 9.092063492063492, typ=False)
+add_region('marka', 10.054965986394558, typ=False)
+add_region('marka', 10.337959183673469, typ=False)
